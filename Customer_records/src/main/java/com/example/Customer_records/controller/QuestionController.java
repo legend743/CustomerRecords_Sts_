3c56102a7f6dd8e02f44d7b04dcd3b51531e2521
faisal <mohd.faisal@blubook.co.in>
2024-01-25 01:11:50 +0530

    api fixed

--- Customer_records/src/main/java/com/example/Customer_records/controller/QuestionController.java
+++ Customer_records/src/main/java/com/example/Customer_records/controller/QuestionController.java
@@ -1,5 +1,26 @@
 package com.example.Customer_records.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Customer_records.entity.Question;
+import com.example.Customer_records.service.QuestionService;
+
+@RestController
 public class QuestionController {
+	@Autowired
+	private QuestionService questionService;
+	@PostMapping("/Question")
+	public ResponseEntity<Question> AddQuestion(@RequestBody Question question) {
+		Question createdquestion=questionService.createQuestion(question);
+		
+		return new ResponseEntity<>(createdquestion,HttpStatus.CREATED);
+		
+	}
 
 }
--- Customer_records/src/main/java/com/example/Customer_records/service/QuestionService.java
+++ Customer_records/src/main/java/com/example/Customer_records/service/QuestionService.java
@@ -1,9 +1,11 @@
 package com.example.Customer_records.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.Customer_records.dao.QuestionDao;
 import com.example.Customer_records.entity.Question;
+@Service
 
 public class QuestionService {
 	@Autowired
